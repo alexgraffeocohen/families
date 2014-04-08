47305957dcc6587716a58b563b23080b43cee6d5
asullivan210 <ariellejsullivan@gmail.com>
2014-04-08 16:33:37 -0400

    album routes

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,5 @@
 class AlbumsController < ApplicationController
-  before_action :set_album, only: [:show, :edit]
+  before_action :set_album, only: [:show, :edit, :update, :destroy]
   
   def show
   end
@@ -10,6 +10,29 @@ class AlbumsController < ApplicationController
 
   def new
     @album = Album.new
+    render 'form'
+  end
+
+  def create
+    album = Album.create(album_params)
+    redirect_to album_path(album)
+
+  end
+
+  def update
+    @album.update(album_params)
+
+    if @album.save
+      redirect_to @album, :notice => "User successfully edited"
+    else
+      render 'form' 
+      flash[:alert] = "Sorry, could not update."
+    end
+  end
+
+  def destroy
+    @album.destroy
+    redirect_to albums_path
   end
 
   def index
@@ -20,5 +43,9 @@ class AlbumsController < ApplicationController
 
   def set_album
     @album = Album.find(params[:id])
+  end
+
+  def album_params
+    params.require(:album).permit(:name, :date)
   end
 end
