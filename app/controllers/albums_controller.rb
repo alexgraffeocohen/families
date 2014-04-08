af7d13f67f6054c79b06cb626685e496a0ddf2fe
asullivan210 <ariellejsullivan@gmail.com>
2014-04-08 15:43:24 -0400

    views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,24 @@
 class AlbumsController < ApplicationController
+  before_action :set_album, only: [:show, :edit]
+  
+  def show
+  end
+
+  def edit
+    render 'form'
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def index
+    @albums = Album.all
+  end
+
+  private
+
+  def set_album
+    @album = Album.find(params[:id])
+  end
 end
