807bc5a1a33469ad80cb47944e0dbffeedc22da2
alexwilkinson <alex.wilkinson@me.com>
2014-04-08 10:11:14 -0400

    initial commit

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem 'figaro', git: 'https://github.com/laserlemon/figaro.git'
+
 group :test, :development do
   gem "rspec-rails"
   gem "capybara"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+      when :notice then "alert alert-info alert-dismissable"
+      when :success then "alert alert-success alert-dismissable"
+      when :warning then "alert alert-warning alert-dismissable"
+      when :alert then "alert alert-danger alert-dismissable"
+    end
+  end
+  
 end
