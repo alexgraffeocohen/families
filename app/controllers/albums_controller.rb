6107532fce48a7ee690290a7cf208b856382ece7
danielspector <danielyspector@gmail.com>
2014-04-09 10:06:29 -0400

    read my errors

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -37,7 +37,6 @@ class AlbumsController < ApplicationController
         f.html {redirect_to albums_path}
         f.js {render 'destroy', locals: {album: @album, family: @family}}
       else
-        flash[:alert] = "Sorry, you do not own this album."
         f.js {render 'destroy_failure'}
       end
     end
