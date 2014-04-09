a2ca6d145b61b1f70c80474ea0e08fd939ca4ea4
asullivan210 <ariellejsullivan@gmail.com>
2014-04-09 16:09:29 -0400

    photo ajax

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,19 @@
 class PhotosController < ActionController::Base
+  before_action :set_photo, only: [:create, :destroy]
 
   def create
     @photo = Photo.new(photo_params)
-    @photo.album_id = params[:album_id]
+    @photo.album_id = @album.id
     @photo.save
   end
 
   def destroy
-    @photo = Photo.find(params[:photo_id])
-    @album = Album.find(params[:album_id])
+
     respond_to do |f|
       if current_person == @album.owner
         @photo.destroy
         f.html {redirect_to albums_path}
-        f.js {render 'destroy', locals: {album: @album, photo: @photo}}
+        f.js {render 'destroy', locals: {family: @family, album: @album, photo: @photo}}
       else
         @msg = "Sorry, you do not own this album."
         f.js {render 'destroy_failure', locals: {msge: @msg}}
@@ -22,6 +22,12 @@ class PhotosController < ActionController::Base
   end
 
   private
+
+  def set_photo
+    @photo = Photo.find(params[:photo_id]) if params[:photo_id]
+    @album = Album.find(params[:album_id])
+    @family = Family.find(params[:id])
+  end
 
   def photo_params
     params.require(:photo).permit(:caption, :data)
