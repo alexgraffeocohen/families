0b95d60900942325a1d4952522020a14137b1e61
asullivan210 <ariellejsullivan@gmail.com>
2014-04-09 14:18:30 -0400

    set family back in

--- app/controllers/family_controller.rb
+++ app/controllers/family_controller.rb
@@ -2,21 +2,23 @@ class FamilyController < ApplicationController
   before_filter :authenticate_person!
   before_action :set_family, only: [:show, :about_us]
 
-  def index
-  end
-
   def new
     @family = Family.new
   end
 
-  def show
-    @family = Family.find(params[:id])
+  def show 
   end
 
   def create
   end
 
   def about_us
+  end
+
+  private
+
+  def set_family
+    @family = Family.find(params[:id])
   end
 
 end
