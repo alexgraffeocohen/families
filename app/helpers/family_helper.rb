460912231daa1bd9e9bbca69bfc68c67b6936e8f
danielspector <danielyspector@gmail.com>
2014-04-10 13:01:40 -0400

    updated views for sign in and sign up. still need to do confirmations

--- app/helpers/family_helper.rb
+++ app/helpers/family_helper.rb
@@ -1,2 +1,8 @@
 module FamilyHelper
+  def create_accounts(params, family)
+    params[:people][:emails].each do |account| 
+      family.person_families.build(person: Person.create(email: account))
+      family.save
+    end
+  end
 end
