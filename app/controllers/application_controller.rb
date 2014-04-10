4afd6a5a2fe0a801e4fb635a2b6f23c274d91015
danielspector <danielyspector@gmail.com>
2014-04-10 10:11:36 -0400

    updated forms and added admin ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  # def after_sign_in_path_for(resource)
-  #   current_user_path
-  # end
-
-  # def after_sign_up_path_for(resource)
-  #   redirect_to new_family_path
-  # end
   
   private
     def configure_permitted_parameters
-      [:first_name, :age, :birthday, :data].each do |param|
+      #this is a total security risk. need to revisit.
+      [:first_name, :age, :birthday, :data, :admin, :gender].each do |param|
         devise_parameter_sanitizer.for(:sign_up) << param
         devise_parameter_sanitizer.for(:account_update) << param
       end
