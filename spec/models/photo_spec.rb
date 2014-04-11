031020e138715a051826fae2b996649f2c19a003
asullivan210 <ariellejsullivan@gmail.com>
2014-04-11 16:05:31 -0400

    more specs

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe Photo do
-  it "has an uploader" do
+  before do
+    @photo = create(:photo)
+    @album = create(:album)
+    @album.photos << @photo
   end
-
+  
   it "belongs to an album" do
+    expect(@photo.album).to eq(@album)
   end
 end
