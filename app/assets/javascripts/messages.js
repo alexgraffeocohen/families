086cf5c8a598698273e62466b34c0392ee9a999f
asullivan210 <ariellejsullivan@gmail.com>
2014-04-14 15:20:20 -0400

    live messaging

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -6,7 +6,7 @@ $(function(){
 
 function checkNewMessages () {
   var convo_id = $("h2").data("id")
-  var after = $(".message").last().data("time");
+  var after = $(".message").first().data("time");
   $.getScript("messages.js?conversation_id=" + convo_id + "&after=" + after);
   setTimeout(checkNewMessages, 5000);
 }
