097f891a081ca7000d845967550bc73d9cb7c9a9
danielspector <danielyspector@gmail.com>
2014-04-20 13:54:03 -0400

    family updated

--- app/assets/javascripts/family.js
+++ app/assets/javascripts/family.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#member_inputs").on("click", "a.delete_x_member_input", function(e){
+  $(document).on("click", "a.delete_x_member_input", function(e){
     e.preventDefault();
     $(this).closest(".member_input").remove();
   });
