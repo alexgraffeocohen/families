f7f6c36e42b64a46245727407d3615553ed2d626
asullivan210 <ariellejsullivan@gmail.com>
2014-04-21 14:12:20 -0400

    member input delete x bug

--- app/assets/javascripts/family.js
+++ app/assets/javascripts/family.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   $(document).on("click", "a.delete_x_member_input", function(e){
     e.preventDefault();
-    $(this).closest(".member_input").remove();
+    if($("a.delete_x_member_input").size() > 1){
+      $(this).closest(".member_input").remove();
+    }
   });
 });
   
