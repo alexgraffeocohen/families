65bf98ec5d857e1da29eb2e1569bb618ecac777c
alexwilkinson <alex.wilkinson@me.com>
2014-04-22 22:56:49 -0400

    partially simplified checkbox methods

--- app/models/person.rb
+++ app/models/person.rb
@@ -152,22 +152,24 @@ class Person < ActiveRecord::Base
     self.age = DateTime.now.year - self.birthday.year if self.birthday
   end
 
+  def parametize(relationship)
+    relationship.gsub("-", "_").pluralize
+  end
+
   def singular_checkbox_hash(relationships, index, hash)
-    if self.send(relationships[0])
-      hash[index+1] = [self.send(relationships[0]).permission_slug]
-    end
-    if relationships[1] && self.send(relationships[1])
-      hash[index+1] = [] if hash[index+1].nil?
-      (hash[index+1] << self.send(relationships[1]).permission_slug)
-    end
+    hash[index+1] = [self.send(relationships[0]).try(:permission_slug)]
+    hash[index+1] = [] if hash[index+1].nil?
+    hash[index+1] << self.send(relationships[1]).try(:permission_slug)
   end
 
   def group_checkbox_hash(relationships, index, hash)
-    if self.send(relationships[0].gsub("-", "_").pluralize)
-      hash[index+1] = self.send(relationships[0].gsub("-", "_").pluralize).map {|rel| rel.permission_slug}
+    
+    hash[index+1] = self.send(parametize(relationships[0])).try(:map) do |rel| 
+      rel.permission_slug
     end
-    if relationships[1] && self.send(relationships[1].gsub("-", "_").pluralize)
-      hash[index+1] << self.send(relationships[1].gsub("-", "_").pluralize).map {|rel| rel.permission_slug}
+    
+    if relationships[1] && self.send(parametize(relationships[1]))
+      hash[index+1] << self.send(parametize(relationships[1])).map {|rel| rel.permission_slug}
     end
   end
 end
