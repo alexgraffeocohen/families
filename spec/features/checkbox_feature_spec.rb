0ac296dd5aa5caef4b563618ebbec02c386338d3
Arielle Sullivan <ariellejsullivan@gmail.com>
2014-04-28 14:00:21 -0400

    new computer, checkboxspec working

--- spec/features/checkbox_feature_spec.rb
+++ spec/features/checkbox_feature_spec.rb
@@ -8,6 +8,8 @@ feature "Checkboxes as Greg" do
   before :each do
     make_brady_bunch
     login_as(@greg, :scope => :person)
+    visit "/families/#{@brady.name_slug}/conversations"
+    find_link("By Person").trigger('click')
   end
 
   after :each do
@@ -15,38 +17,33 @@ feature "Checkboxes as Greg" do
   end
 
   scenario "sibling checkbox", js: true do
-    puts @brady.name_slug
-     visit "/"
-     find_link("Our Conversations").trigger('click')
-     page.check('conversation_parse_permission_1')
-     sleep 3
-     expect(find('#conversation_parse_permission_1').checked?).to eq(true)
-     expect(find("input#conversation_parse_permission_#{@cindy.permission_slug}").checked?).to eq(true)
-     # expect(find("#conversation_parse_permission_#{@carol.permission_slug}").checked?).to eq(false)
+     # visit "/"
+     # find_link("Our Conversations").trigger('click')
+    page.check('conversation_parse_permission_1')
+    expect(find('#conversation_parse_permission_4').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@cindy.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@carol.permission_slug}").checked?).to eq(false)
   end
 
-  xscenario "parent checkbox", js: true do
-     visit "families/#{@brady.name_slug}/conversations"
-     page.check('conversation_parse_permission_2')
-     click_link('By Person')
-     page.has_checked_field?('conversation_parse_permission_mike#{@mike.id}')
-     page.has_unchecked_field?('conversation_parse_permission_peter#{@peter.id}')
+  scenario "parent checkbox", js: true do
+    page.check('conversation_parse_permission_2')
+    expect(find('#conversation_parse_permission_9').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@mike.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@marcia.permission_slug}").checked?).to eq(false)
   end
 
-  xscenario "grandparent checkbox", js: true do
-     visit "families/#{@brady.name_slug}/conversations"
-     page.check('conversation_parse_permission_4')
-     click_link('By Person')
-     page.has_checked_field?('conversation_parse_permission_connie#{@connie.id}')
-     page.has_unchecked_field?('conversation_parse_permission_jenny#{@jenny.id}')
+  scenario "grandparent checkbox", js: true do
+    page.check('conversation_parse_permission_4')
+    expect(find('#conversation_parse_permission_1').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@connie.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@jon.permission_slug}").checked?).to eq(false)
   end
 
-  xscenario "aunts/uncles checkbox", js: true do
-     visit "families/#{@brady.name_slug}/conversations"
-     page.check('conversation_parse_permission_9')
-     click_link('By Person')
-     page.has_checked_field?('conversation_parse_permission_jon#{@jon.id}')
-     page.has_unchecked_field?('conversation_parse_permission_marcia#{@marcia.id}')
+  scenario "aunts/uncles checkbox", js: true do
+    page.check('conversation_parse_permission_9')
+    expect(find('#conversation_parse_permission_2').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@jenny.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@bobby.permission_slug}").checked?).to eq(false)
   end
 end
 
@@ -54,33 +51,59 @@ feature "Checkboxes as Connie" do
   before :each do
     make_brady_bunch
     login_as(@connie, :scope => :person)
+    visit "/families/#{@brady.name_slug}/conversations"
+    find_link("By Person").trigger('click')
   end
 
   after :each do
     Warden.test_reset!
   end
 
-  xscenario "grandchild checkbox", js: true do
-     visit "families/#{@brady.name_slug}/conversations"
-     page.check('conversation_parse_permission_5')
-     click_link('By Person')
-     page.has_checked_field?('conversation_parse_permission_mike#{@mike.id}')
-     page.has_unchecked_field?('conversation_parse_permission_peter#{@peter.id}')
+  scenario "grandchild checkbox", js: true do
+    page.check('conversation_parse_permission_5')
+    expect(find('#conversation_parse_permission_3').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@marcia.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@mike.permission_slug}").checked?).to eq(false)
   end
 
-  xscenario "children checkbox", js: true do
-     visit "families/#{@brady.name_slug}/conversations"
-     page.check('conversation_parse_permission_3')
-     click_link('By Person')
-     page.has_checked_field?('conversation_parse_permission_connie#{@connie.id}')
-     page.has_unchecked_field?('conversation_parse_permission_jenny#{@jenny.id}')
+  scenario "children checkbox", js: true do
+    page.check('conversation_parse_permission_3')
+    expect(find('#conversation_parse_permission_5').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@carol.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@jan.permission_slug}").checked?).to eq(false)
   end
 
-  xscenario "children-in-law checkbox", js: true do
-     visit "families/#{@brady.name_slug}/conversations"
-     page.check('conversation_parse_permission_6')
-     click_link('By Person')
-     page.has_checked_field?('conversation_parse_permission_jon#{@jon.id}')
-     page.has_unchecked_field?('conversation_parse_permission_marcia#{@marcia.id}')
+  scenario "children-in-law checkbox", js: true do
+    page.check('conversation_parse_permission_6')
+    expect(find('#conversation_parse_permission_3').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@mike.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@carol.permission_slug}").checked?).to eq(false)
+  end
+end
+
+feature "Checkboxes as Mike" do
+  before :each do
+    make_brady_bunch
+    login_as(@mike, :scope => :person)
+    visit "/families/#{@brady.name_slug}/conversations"
+    find_link("By Person").trigger('click')
+  end
+
+  after :each do
+    Warden.test_reset!
+  end
+
+  scenario "sibling in laws checkbox", js: true do
+    page.check('conversation_parse_permission_12')
+    expect(find('#conversation_parse_permission_8').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@jon.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@carol.permission_slug}").checked?).to eq(false)
+  end
+
+  scenario "parent in laws checkbox", js: true do
+    page.check('conversation_parse_permission_7')
+    expect(find('#conversation_parse_permission_3').checked?).to eq(false)
+    expect(find("#conversation_parse_permission_#{@harold.permission_slug}").checked?).to eq(true)
+    expect(find("#conversation_parse_permission_#{@jenny.permission_slug}").checked?).to eq(false)
   end
 end
